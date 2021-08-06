0c8ee475a7112bfd569957bef07460abb15f621f
Kevin Salina <kevin.salina25@gmail.com>
2021-08-06 14:28:37 -0400

    Created Seasons page. Added basic styling

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const showData = require('./showdata')
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
-
+// Home Page
 app.get('/', (req, res) => {
   res.render('index', { show: showData })
+})
+
+// Season Route
+app.get('/season/:id', (req, res) => {
+  // Getting Variables
+  const seasonNumber = req.params.id
+  const showTitle = showData.title
+
+  res.render('season', { season: showData.seasons[seasonNumber - 1], showTitle })
 })
 
 app.listen(3000, () => {
