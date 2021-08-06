c5d05ecc0a8a126ad7d6669cb3ceb66b166e0443
Kevin Salina <kevin.salina25@gmail.com>
2021-08-06 13:19:22 -0400

    Updated file structure. Created Homepage

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
+const showData = require('./showdata')
+
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
-  res.send("This is my app")
+  res.render('index', { show: showData })
 })
 
 app.listen(3000, () => {
